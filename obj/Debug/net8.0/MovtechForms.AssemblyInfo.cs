e34289620e1bd707c01a1250821a1ef974437094
Juan Feliciano <juanfeliciasantos@gmail.com>
2024-08-07 16:25:47 -0300

    fix: bugs fixed

--- obj/Debug/net8.0/MovtechForms.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovtechForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovtechForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe4967efab48f03af45e7db07bd24cb4db7cca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b405c9166cecf1859b8f10a3a471fc9e18d202")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovtechForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovtechForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
